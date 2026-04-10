b26c8f524ec14cb134a5cdbb3de38c963790c0a1
maleykaaliyeva <meleykeliyeva16@gmail.com>
2026-04-10 14:41:39 +0000

    4. Access and update private attribute

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module for creating Square class"""
+
+
+class Square:
+    """A class representing Square"""
+
+    def __init__(self, size=0):
+        """Instantiation with a given size"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter for the size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for the size of square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return of the area of square"""
+        return self.__size ** 2
